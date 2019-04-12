87e53a880eeaa4a4ac51b85102213fba891e926d
Joaquin Palominos <japalominos@miuandes.cl>
2019-04-11 20:17:54 -0400

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+  resources :users do
+      member do
+          get 'user_with_most_tickets'
+      end
+  end
+  resources :event_venues
+    
+  resources :events do
+      resources :ticket_types do
+        resources :tickets
+      end
+  end
 end
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
